13980b13c05ebed9d8f7d7214761cf6fa935c2b0
DanielLChang <daniellawrencechang@gmail.com>
2017-03-12 15:05:42 -0700

    Disable buttons

--- app/components/ReservationsIndex/ReservationItem.js
+++ app/components/ReservationsIndex/ReservationItem.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { ScrollView, View, Text, Image, Alert } from 'react-native';
+import { ScrollView, View, Text, Image, Alert, TouchableOpacity } from 'react-native';
 
 import { SmallButton } from '../common';
 
 class RestaurantItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      buttonOne: false,
+      buttonTwo: false,
+      buttonThree: false,
+      buttonFour: false,
+    };
   }
 
   render() {
@@ -26,52 +33,57 @@ class RestaurantItem extends React.Component {
         </View>
         <View style={styles.buttonContainer}>
           <View style={styles.itemButton}>
-            <SmallButton
-              buttonText={'8:00 PM'}
-              onPress={() => Alert.alert(
-                `${this.props.res.name}`,
-                `Join the party for 8:00PM?`,
-                [
-                  {text: 'No', onPress: () => console.log('Cancel Pressed!')},
-                  {text: 'Yes', onPress: () => console.log('OK Pressed!')},
-                ]
-              )}
-              />
-            <SmallButton
-              buttonText={'8:15 PM'}
-              onPress={() => Alert.alert(
-                `${this.props.res.name}`,
-                `Join the party for 8:15PM?`,
-                [
-                  {text: 'No', onPress: () => console.log('Cancel Pressed!')},
-                  {text: 'Yes', onPress: () => console.log('OK Pressed!')},
-                ]
-              )}
-              />
+            <TouchableOpacity style={[styles.button, this.state.buttonOne && styles.buttonAlt]}
+              onPress={() => Alert.alert(`${this.props.res.name}`,
+                                          `Join the party for 8:00PM?`,
+                                          [
+                                            {text: 'No', onPress: () => console.log('Cancel Pressed!')},
+                                            {text: 'Yes', onPress: () => this.setState({buttonOne: true})},
+                                          ]
+            )}>
+              <Text style={styles.buttonText}>
+                {'8:00 PM'}
+              </Text>
+            </TouchableOpacity>
+              <TouchableOpacity style={[styles.button, this.state.buttonTwo && styles.buttonAlt]}
+                onPress={() => Alert.alert(`${this.props.res.name}`,
+                                            `Join the party for 8:15PM?`,
+                                            [
+                                              {text: 'No', onPress: () => console.log('Cancel Pressed!')},
+                                              {text: 'Yes', onPress: () => this.setState({buttonTwo: true})},
+                                            ]
+              )}>
+                <Text style={styles.buttonText}>
+                  {'8:15 PM'}
+                </Text>
+              </TouchableOpacity>
           </View>
           <View style={styles.itemButton}>
-            <SmallButton
-              buttonText={'8:30 PM'}
-              onPress={() => Alert.alert(
-                `${this.props.res.name}`,
-                `Join the party for 8:30PM?`,
-                [
-                  {text: 'No', onPress: () => console.log('Cancel Pressed!')},
-                  {text: 'Yes', onPress: () => console.log('OK Pressed!')},
-                ]
-              )}
-              />
-            <SmallButton
-              buttonText={'8:45 PM'}
-              onPress={() => Alert.alert(
-                `${this.props.res.name}`,
-                `Join the party for 8:45PM?`,
-                [
-                  {text: 'No', onPress: () => console.log('Cancel Pressed!')},
-                  {text: 'Yes', onPress: () => console.log('OK Pressed!')},
-                ]
-              )}
-              />
+            <TouchableOpacity style={[styles.button, this.state.buttonThree && styles.buttonAlt]}
+              onPress={() => Alert.alert(`${this.props.res.name}`,
+                                          `Join the party for 8:30PM?`,
+                                          [
+                                            {text: 'No', onPress: () => console.log('Cancel Pressed!')},
+                                            {text: 'Yes', onPress: () => this.setState({buttonThree: true})},
+                                          ]
+            )}>
+              <Text style={styles.buttonText}>
+                {'8:30 PM'}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, this.state.buttonFour && styles.buttonAlt]}
+              onPress={() => Alert.alert(`${this.props.res.name}`,
+                                          `Join the party for 8:45PM?`,
+                                          [
+                                            {text: 'No', onPress: () => console.log('Cancel Pressed!')},
+                                            {text: 'Yes', onPress: () => this.setState({buttonFour: true})},
+                                          ]
+            )}>
+              <Text style={styles.buttonText}>
+                {'8:45 PM'}
+              </Text>
+            </TouchableOpacity>
+
           </View>
         </View>
       </View>
@@ -110,6 +122,37 @@ const styles = {
   },
   city: {
     color: '#343435'
+  },
+  button: {
+    marginTop: '7%',
+    // alignSelf: 'center',
+    height: 30,
+    width: 150,
+    backgroundColor: '#ed6d72',
+    paddingLeft: 40,
+    paddingRight: 40,
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginRight: 20
+  },
+  buttonText: {
+    zIndex: 30,
+    fontSize: 16,
+    color: 'white',
+    fontFamily: 'Arial',
+    letterSpacing: 0.5
+  },
+  buttonAlt: {
+    marginTop: '7%',
+    // alignSelf: 'center',
+    height: 30,
+    width: 150,
+    backgroundColor: '#f7a5a9',
+    paddingLeft: 40,
+    paddingRight: 40,
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginRight: 20,
   }
 };
 
